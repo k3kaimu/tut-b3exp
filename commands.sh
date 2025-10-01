0ab766d921303c912defb621c5d74262b0da841e
Komatsu Kazuki <komatsu.kazuki.op@tut.jp>
2025-10-01 14:58:53 +0900

    fix hackrf settings

--- commands.sh
+++ commands.sh
@@ -1,1 +1,2 @@
+source venv/bin/activate
 alias spawn_ezsdr="docker run -i --rm --init --net=host $(lsusb | grep "HackRF" | awk '{printf "--device=/dev/bus/usb/%03d/%03d ", $2, $4}') -v $(pwd)/client:/exported ghcr.io/k3kaimu/ezsdr:v3.0.16"
